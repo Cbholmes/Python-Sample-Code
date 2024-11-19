2eadc11fd61d54da914a1805f5e1bd9156059399
Cbholmes <cbholmes03@gmail.com>
2024-11-19 09:49:10 -0600

    Update admin.py
    Update admin module to ver 2 with privleges as a seperate class

--- User and Children/admin.py
+++ User and Children/admin.py
@@ -5,15 +5,21 @@ class Admin(User):
                  dob="", email="", loginattempts=0):
         """Constructor, creates an admin with given user arguments and privileges"""
         super().__init__(firstname, lastname, username, dob, email, loginattempts)
-        self._privileges = privileges
+        self._privileges = Privileges(privileges)
 
+class Privileges():
+    """Represents Privleges an admin has"""
+    def __init__(self, privileges):
+        self._privileges = privileges
+        
     def showPrivileges(self):
         print("Current privileges:")
         for privilege in self._privileges:
             print(privilege)
+    
 
 details = {"firstname":"John","lastname":"Doe",
            "privileges":["R Users\\*", "RW AdminDocs", "RWX AdminTools"],
            "dob":"January 1st, 2000"}
 adminuser = Admin(**details)
-adminuser.showPrivileges()
+adminuser._privileges.showPrivileges()
