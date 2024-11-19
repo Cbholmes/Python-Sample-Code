04bb04b7479a2bb85a7d40dfa2b371fd50ba5460
Cbholmes <cbholmes03@gmail.com>
2024-11-19 09:44:48 -0600

    Update user.py
    Update to ver 2 of the user module

--- User and Children/user.py
+++ User and Children/user.py
@@ -1,6 +1,6 @@
 class User():
     """Object represents a User."""
-    def __init__(self, firstname, lastname, username="", dob="", email=""):
+    def __init__(self, firstname, lastname, username="", dob="", email="", loginattempts = 0):
         """Constructor, creates a user with the given first and last names,
             username (lowercase), date of birth, and email. If username is blank username = firstname.lastname."""
         self._firstname = firstname
@@ -8,14 +8,26 @@ class User():
         self._username = (firstname + "." + lastname).lower() if username == "" else username.lower()
         self._dob=dob
         self._email=email
+        self._loginattempts = loginattempts
+
     def describeUser(self):
         """Prints details of the User."""
         print("User Details:")
         print("\tFirst Name:", self._firstname)
         print("\tLast Name:", self._lastname)
         print("\tUsername:", self._username)
-        print("\tDate of Birth:", self._dob)
-        print("\tEmail Address:", self._email)
+        if self._dob != "": print("\tDate of Birth:", self._dob)
+        if self._email != "": print("\tEmail Address:", self._email)
+
     def greetUser(self):
         """Prints a greeting to the User."""
         print("Hello there", self._firstname + ".")
+
+    def incrLoginAttempts(self):
+        """Increments amount of login attempts by 1."""
+        self._loginattempts += 1
+
+    def resetLoginAttempts(self):
+        """Sets amount of login attempts to 0."""
+        self._loginattempts = 0
+
