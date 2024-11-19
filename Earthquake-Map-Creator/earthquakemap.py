8ea6e437acdbeffbbc7f09c0538db0c3afcd9f18
Cbholmes <cbholmes03@gmail.com>
2024-11-19 11:02:27 -0600

    Update earthquakemap.py comments

--- Earthquake-Map-Creator/earthquakemap.py
+++ Earthquake-Map-Creator/earthquakemap.py
@@ -33,6 +33,7 @@ class EarthquakeMap():
         
         #creates a list of plot point sizes converted from the 
         #magnitude of the point proportionally to a scale from 1-6
+        #with 1 being the minimum magnitude in the data frame and 6 being the max
         psizes = []
         magdiff = max(df["mag"]) - min(df["mag"])
         sizediff = 5
