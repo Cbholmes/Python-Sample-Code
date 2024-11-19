298a986e2202325c4028347d37f8b966d0675de0
Cbholmes <cbholmes03@gmail.com>
2024-11-19 09:47:02 -0600

    Upload Admin
    Upload Admin Module which is a child of the User ver 2 module

---
+++ User and Children/admin.py
@@ -0,0 +1,19 @@
+from user import User
+class Admin(User):
+    """Represents an Admin as a child of User"""
+    def __init__(self, firstname, lastname, privileges, username="",
+                 dob="", email="", loginattempts=0):
+        """Constructor, creates an admin with given user arguments and privileges"""
+        super().__init__(firstname, lastname, username, dob, email, loginattempts)
+        self._privileges = privileges
+
+    def showPrivileges(self):
+        print("Current privileges:")
+        for privilege in self._privileges:
+            print(privilege)
+
+details = {"firstname":"John","lastname":"Doe",
+           "privileges":["R Users\\*", "RW AdminDocs", "RWX AdminTools"],
+           "dob":"January 1st, 2000"}
+adminuser = Admin(**details)
+adminuser.showPrivileges()
